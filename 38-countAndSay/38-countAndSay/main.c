1e048e9f5012f5f3daf33e1a598c016e4acc6239
FlyElephant <1061731917@qq.com>
2017-09-08 21:33:07 +0800

    读取数字

--- 38-countAndSay/38-countAndSay/main.c
+++ 38-countAndSay/38-countAndSay/main.c
@@ -13,25 +13,20 @@
 char* countAndSay(int n) {
     char *pre = "1";
     for (int i = 2; i <= n; i++) {
-        char *tmp =  (char *)malloc(1 + strlen(pre));;
-        // 上一个字符串
+        int len = (int)strlen(pre)*2 + 1;
+        char *tmp = (char*)malloc(len*sizeof(char));
+        int m = 0;
         for (int j = 0; j < strlen(pre); j++) {
             char cur = *(pre + j);
-            char next = *(pre + j + 1);
-            if (next != '\0') {
-                if (cur != next) {
-                    strcat(tmp, "1");
-                    strcat(tmp, &cur);
-                } else {
-                    strcat(tmp, "2");
-                    strcat(tmp, &cur);
-                    j++;
-                }
-            } else {
-                strcat(tmp, "1");
-                strcat(tmp, &cur);
+            int k = j;
+            while (cur == *(pre + k)) {
+                k++;
             }
+            tmp[m++] = (k - j) + '0';
+            tmp[m++] = cur;
+            j = k - 1;
         }
+        tmp[m] = '\0';
         pre = tmp;
     }
     return pre;
@@ -40,17 +35,20 @@ char* countAndSay(int n) {
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
-    char *res = countAndSay(4);
-    printf("最后的结果:%s\n",res);
+    for (int i = 0; i <= 10; i++) {
+        char *res = countAndSay(i);
+        printf("%d的表达结果:%s---\n",i,res);
+    }
+
     
-    char strDest[6];
-    char * strA = "abc";
-    strcpy(strDest, strA);
-    strncat(strDest, "123", 2);
-    printf("%s \n", strDest); // 打印出：abc12
-    
-    char *test = "";
-    char num = '2';
+//    char strDest[6];
+//    char * strA = "abc";
+//    strcpy(strDest, strA);
+//    strncat(strDest, "123", 2);
+//    printf("%s \n", strDest); // 打印出：abc12
+//    
+//    char *test = "";
+//    char num = '2';
     
     return 0;
 }
