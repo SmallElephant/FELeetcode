8cfcf8a47ff879c831dc62ecbb10b3bd399759ce
FlyElephant <1061731917@qq.com>
2017-09-09 09:27:49 +0800

    最大子序列n的平法解法

--- 53-maxSubArray/53-maxSubArray/main.c
+++ 53-maxSubArray/53-maxSubArray/main.c
@@ -23,11 +23,25 @@ int maxSubArray(int* nums, int numsSize) {
     return maxSum;
 }
 
+int maxSubArray2(int* nums, int numsSize) {
+    int maxSum = 0;// 最大值
+    for (int i = 0; i < numsSize; i++) {
+        int curSum = 0;
+        for (int j = i; j < numsSize; j++) {
+            curSum += *(nums + j);
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+        }
+    }
+    return maxSum;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     int nums[9] = {-2,1,-3,4,-1,2,1,-5,4};
-    int res = maxSubArray(nums, 9);
+    int res = maxSubArray2(nums, 9);
     printf("结果:%d\n",res);
     return 0;
 }
