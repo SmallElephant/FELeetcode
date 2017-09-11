c5ef1acabb2674ba98abe9d4e8e90d00e807feb6
xiangdefei <xiangdefei@we.com>
2017-09-11 19:08:38 +0800

    C++ 二叉树的层次遍历

--- 107-levelOrderBottom/107-levelOrderBottom/main.cpp
+++ 107-levelOrderBottom/107-levelOrderBottom/main.cpp
@@ -29,7 +29,8 @@ public:
         queue.push(root);
         while (!queue.empty()) {
             vector<int> vi;
-            for (int i = 0; i < queue.size(); i++) {
+            int size = (int)queue.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode *node = queue.front();
                 queue.pop();
                 if (node->left != NULL) {
@@ -42,38 +43,20 @@ public:
             }
             vvi.push_back(vi);
         }
+        reverse(vvi.begin(), vvi.end());
         return vvi;
     }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, World!\n";
-    int n[] = {1, 2, 3, 4, 5} ;
-    vector<int> V;
-    vector<int> a(n, n+5);              //将数组n的前5个元素作为向量a的初值
-    for (int i = 0; i < a.size(); i++) {
-        cout << a[i]<<endl;
-    }
-    
-    vector<vector<int>> test;
-    vector<int> test1(n, n+2);
-    vector<int> test2(n, n+3);
-    for (int i = 0; i < test.size(); i++) {
-        vector<int> tmp = test[i];
-        cout << &tmp << endl;
-//        for (int j = 0; j < tmp.size(); j++) {
-//            
-//        }
-    }
-    
-    queue<int> queue;
-    queue.push(10);
-    queue.push(20);
-    while (!queue.empty()) {
-        cout << queue.front()<<endl;
-        queue.pop();
-    }
+//    cout << "Hello, World!\n";
+//    int n[] = {1, 2, 3, 4, 5} ;
+//    vector<int> V;
+//    vector<int> a(n, n+5);              //将数组n的前5个元素作为向量a的初值
+//    for (int i = 0; i < a.size(); i++) {
+//        cout << a[i]<<endl;
+//    }
     TreeNode node1 = TreeNode(15);
     TreeNode node2 = TreeNode(7);
     TreeNode node3 = TreeNode(9);
@@ -83,5 +66,14 @@ int main(int argc, const char * argv[]) {
     TreeNode node5 = TreeNode(3);
     node5.left = &node3;
     node5.right = &node4;
+    Solution solution;
+    vector<vector<int>> vvi = solution.levelOrderBottom(&node5);
+    for (int i = 0; i < vvi.size(); i++) {
+        vector<int> tmp = vvi[i];
+        for (int j = 0; j < tmp.size(); j++) {
+            cout << tmp[j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
