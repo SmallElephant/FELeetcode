20d5513fc851969cf3477b5666e6bccee2fe4a9a
xiangdefei <xiangdefei@we.com>
2017-09-13 13:58:26 +0800

    杨辉三角一维数组版本

---
+++ 119-getRow/119-getRow/main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  119-getRow
+//
+//  Created by FlyElephant on 2017/9/13.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int** generate(int numRows, int** columnSizes) {
+    if (!columnSizes) {
+        return 0;
+    }
+    if (numRows == 0) {
+        return 0;
+    }
+    int **res = (int **)malloc(sizeof(int *) * numRows);
+    int *columns = (int *)malloc(sizeof(int) * numRows);
+    for (int i = 0; i < numRows; i++) {
+        res[i] = (int *)malloc(sizeof(int) * numRows);
+        columns[i] = i + 1;
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) {
+                res[i][j] = 1;
+            } else {
+                res[i][j] = res[i - 1][j] + res[i - 1][j - 1];
+            }
+        }
+    }
+    *columnSizes = columns;
+    return res;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
